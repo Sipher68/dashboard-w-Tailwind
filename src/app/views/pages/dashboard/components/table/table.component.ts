6ad8c13b7e5c1d9ba0d938eb23d4bc27e19f8165
Hans Indino <sipher_68@yahoo.com>
2023-10-20 10:33:52 +0800

    Fixed Table Section of dashboard & Completed Dashboard Sections

--- src/app/views/pages/dashboard/components/table/table.component.ts
+++ src/app/views/pages/dashboard/components/table/table.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { data } from 'src/data/mockdata';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
   standalone: true,
+  imports: [NgFor],
 })
-export class TableComponent {}
+export class TableComponent {
+  public operators = Object.values(data.Operators);
+}
