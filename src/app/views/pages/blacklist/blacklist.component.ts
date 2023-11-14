cb2c2b629566ebb1e2ecd05e2e9493b4cb18a244
Hans Indino <sipher_68@yahoo.com>
2023-11-14 10:57:52 +0800

    Added search function to blacklist table

--- src/app/views/pages/blacklist/blacklist.component.ts
+++ src/app/views/pages/blacklist/blacklist.component.ts
@@ -2,14 +2,30 @@ import { Component } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { data } from 'src/data/mockdata';
 import { PageHeaderComponent } from '../../components/page-header/page-header.component';
+import { SharedService } from 'src/app/services/shared.service';
+import { SearchComponent } from '../../components/search/search.component';
 
 @Component({
   selector: 'app-blacklist',
   templateUrl: './blacklist.component.html',
   styleUrls: ['./blacklist.component.scss'],
   standalone: true,
-  imports: [PageHeaderComponent, NgFor],
+  imports: [PageHeaderComponent, NgFor, SearchComponent],
 })
 export class BlacklistComponent {
-  public blackLists = Object.values(data.Blacklisted);
+  constructor(private sharedService: SharedService) {}
+  public blackLists: any;
+  public searchValue: any;
+
+  ngOnInit() {
+    this.sharedService.searchValue$.subscribe((searchValue) => {
+      this.searchValue = searchValue;
+
+      this.blackLists = !this.searchValue
+        ? Object.values(data.Blacklisted)
+        : Object.values(data.Blacklisted).filter((x) =>
+            x.Name.toLowerCase().includes(this.searchValue.toLowerCase())
+          );
+    });
+  }
 }
